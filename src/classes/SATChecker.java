e6c4125591232ed4e5bfeb7aa8bb2fa83d46d39a
Paul1005 <mccarliep@gmail.com>
2019-07-24 11:03:53 -0700

    finished first draft version

--- src/classes/SATChecker.java
+++ src/classes/SATChecker.java
@@ -1,26 +1,27 @@
 package classes;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SATChecker {
 
-    public boolean isSatisfiable(ArrayList<String> formulas){
+    Map<Character, Boolean> termDictionary = new HashMap<>();
+    ArrayList<Character> terms = new ArrayList<>();
+
+    public boolean isSatisfiable(ArrayList<String> formulas) {
 
         //gather all terms
-        Map<Character, Boolean> termDictionary = new HashMap<>();
-        ArrayList<Character> terms = new ArrayList<>();
+
         //int numTerms = 0;
 
-        for (String formula :formulas) {
+        for (String formula : formulas) {
             String[] splitFormula = formula.split(" ");
-            for(String term : splitFormula){
+            for (String term : splitFormula) {
                 char[] chars = term.toCharArray();
-                for(char character : chars){
-                    if(Character.isLetter(character)){
-                        if(termDictionary.get(character) == null) {
+                for (char character : chars) {
+                    if (Character.isLetter(character)) {
+                        if (termDictionary.get(character) == null) {
                             //numTerms++;
                             termDictionary.put(character, false);
                             terms.add(character);
@@ -31,19 +32,80 @@ public class SATChecker {
         }
 
         // generate truth table
-        int numRows = (int)Math.pow(2, terms.size());
+        int numRows = (int) Math.pow(2, terms.size());
         Map<Integer, Boolean> booleanMap = new HashMap<>();
         booleanMap.put(0, false);
         booleanMap.put(1, true);
 
 
-        for (int i = 0; i< numRows; i++) {
+        for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < terms.size(); j++) {
-                termDictionary.replace(terms.get(j), booleanMap.get((i/(int) Math.pow(2, j))%2));
+                termDictionary.replace(terms.get(j), booleanMap.get((i / (int) Math.pow(2, j)) % 2));
             }
-            System.out.println(termDictionary.toString());
+
+            //do equations with current terms
+            for (String formula : formulas) {
+
+                String[] splitFormula = formula.split(" ");
+                evaluateFormula(splitFormula);
+
+            }
         }
 
         return false;
+    }
+
+    private boolean evaluateFormula(String[] splitFormula) {
+        boolean term1;
+        boolean term2;
+
+        if (splitFormula[0].equals("true")) {
+            term1 = true;
+        } else if (splitFormula[0].equals("false")) {
+            term1 = false;
+        } else {
+            if (splitFormula[0].toCharArray()[0] == '!') {
+                term1 = !termDictionary.get(splitFormula[0].toCharArray()[1]);
+            } else {
+                term1 = termDictionary.get(splitFormula[0].toCharArray()[0]);
+            }
+        }
+
+        if (splitFormula[2].toCharArray()[0] == '!') {
+            term2 = !termDictionary.get(splitFormula[2].toCharArray()[1]);
+        } else {
+            term2 = termDictionary.get(splitFormula[2].toCharArray()[0]);
+        }
+
+        boolean answer = false;
+
+
+        if (splitFormula[1].equals("||")) {
+            answer = term1 || term2;
+        } else if (splitFormula[1].equals("&&")) {
+            answer = term1 && term2;
+        }/* else if(splitFormula[1].equals("?")){
+                return termDictionary.get(term1) ? termDictionary.get(term2);
+        } else if(splitFormula[1].equals("??")){
+                return termDictionary.get(term1) ?? termDictionary.get(term2);
+        }*/
+
+        if (splitFormula.length > 3) {
+            return answer;
+        } else {
+            String[] newSplitFormula = new String[splitFormula.length-2];
+
+            if(answer){
+                newSplitFormula[0] = "true";
+            } else if (!answer){
+                newSplitFormula[0] = "false";
+            }
+
+            for (int i = 3; i<splitFormula.length; i++){
+                newSplitFormula[i-2] = splitFormula[i];
+            }
+
+            return evaluateFormula(newSplitFormula);
+        }
     }
 }
