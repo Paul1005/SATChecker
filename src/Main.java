d5c2aa8cff4d12aec892db8161a624ec1c6c903e
Paul1005 <mccarliep@gmail.com>
2019-07-25 19:07:17 -0700

    implemented simple interface

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
 import classes.SATChecker;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         ArrayList<String> formulas = new ArrayList<>();
-        formulas.add("p && (q || r && (s || t)) && u");
-        //formulas.add("!p && r");
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please enter 1 or more formulas \n" +
+                "Operand symbols are: \n" +
+                "|| for OR \n" +
+                "&& for AND \n" +
+                "? for CONDITIONAL \n" +
+                "?? for BICONDITIONAL \n" +
+                "Use ! for negation \n" +
+                "Put spaces between terms and operands but not between terms and negations or spaces");
+        String formula;
+        do {
+            formula = scanner.nextLine();
+            if(!formula.isEmpty()) {
+                formulas.add(formula);
+            }
+        } while (!formula.isEmpty());
+
         SATChecker satChecker = new SATChecker();
         System.out.println(satChecker.isSatisfiable(formulas));
     }
